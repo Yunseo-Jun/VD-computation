fe124c87be7bdd5f1aea5606ba90cdab1bc1fab9
Yunseo-Jun <zmzm427@snu.ac.kr>
2025-09-16 17:03:33 +0900

    added gatcha

--- Gatcha/sketch.js
+++ Gatcha/sketch.js
@@ -5,12 +5,42 @@ function preload() {
   body = loadImage('body.png');
 
   // get random face
-  face = loadImage(int(random(1, 5)) + ".png");
+  let n = int(random(1, 5));
+  let imgName = n + ".png";  
+  face = loadImage(imgName);
 }
 
 function setup() {
   createCanvas(400, 400);
-  background('yellow');
+
+  let button = createButton('🤗');
+  button.position(10, 10);
+
+  button.mousePressed(() => {
+    let n = int(random(1, 5));
+    let imgName = n + ".png";
+    face = loadImage(imgName);
+  });  
+}
+
+function draw() {
+  background("white");
   image(body, 0, 0);
   image(face, 132, 134);
+
+  if (n === 1) {
+    document.body.style.backgroundColor = "beige";
+    }
+
+  if (n === 2) {
+    document.body.style.backgroundColor = "red";
+    }
+
+  if (n === 3) {
+    document.body.style.backgroundColor = "pink";
+    }
+
+  if (n === 4) {
+    document.body.style.backgroundColor = "skyblue";
+    }
 }
